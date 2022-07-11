98accb7a7e3ff820f4ace659bef6aa3496052bcb
Artem Eroshenko <eroshenkoam@qameta.io>
2022-07-11 15:27:25 +0300

    add rollback command

--- src/main/java/io/github/eroshenkoam/allure/MainCommand.java
+++ src/main/java/io/github/eroshenkoam/allure/MainCommand.java
@@ -5,6 +5,7 @@ import io.github.eroshenkoam.allure.command.ExportTestCasesCommand;
 import io.github.eroshenkoam.allure.command.GitlabSyncGroupsCommand;
 import io.github.eroshenkoam.allure.command.LaunchCleanCommand;
 import io.github.eroshenkoam.allure.command.LdapSyncGroupsCommand;
+import io.github.eroshenkoam.allure.command.RollbackTestCasesCommand;
 import io.github.eroshenkoam.allure.retrofit.GitlabInterceptor;
 import picocli.CommandLine;
 
@@ -15,7 +16,8 @@ import picocli.CommandLine;
                 GitlabSyncGroupsCommand.class,
                 LdapSyncGroupsCommand.class,
                 LaunchCleanCommand.class,
-                ExportTestCasesCommand.class
+                ExportTestCasesCommand.class,
+                RollbackTestCasesCommand.class
         }
 )
 public class MainCommand implements Runnable{
