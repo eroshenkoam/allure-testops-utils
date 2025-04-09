44daa8e6ded764603a42e119200ebd22f9d7cc80
Artem Eroshenko <eroshenkoam@gmail.com>
2025-04-09 21:58:43 +0300

    update java version (via #50)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import org.gradle.internal.os.OperatingSystem;
 plugins {
     java
     application
-    id("com.bmuschko.docker-remote-api") version "7.3.0"
+    id("com.bmuschko.docker-remote-api") version "9.4.0"
     id("org.graalvm.buildtools.native") version "0.9.28"
 }
 
@@ -14,10 +14,10 @@ group = "io.github.eroshenkoam.allure"
 version = version
 
 application {
-    mainClassName = "io.github.eroshenkoam.allure.AllureTestOpsUtils"
+    mainClass.set("io.github.eroshenkoam.allure.AllureTestOpsUtils")
 }
 
-val prepareDockerOutput by tasks.creating {
+val prepareDockerOutput by tasks.registering {
     group = "Build"
     dependsOn(tasks.distZip)
 
@@ -26,7 +26,7 @@ val prepareDockerOutput by tasks.creating {
     }
 }
 
-val prepareDockerfile by tasks.creating(Dockerfile::class) {
+val prepareDockerfile by tasks.registering(Dockerfile::class) {
     group = "Build"
     dependsOn(prepareDockerOutput)
     destFile.set(project.file("build/docker/Dockerfile"))
@@ -42,13 +42,11 @@ val prepareDockerfile by tasks.creating(Dockerfile::class) {
     workingDir("/opt/allure-testops")
 }
 
-val buildDockerImage by tasks.creating(DockerBuildImage::class) {
+val buildDockerImage by tasks.registering(DockerBuildImage::class) {
     group = "Build"
     dependsOn(prepareDockerfile)
     inputDir.set(project.file("build/docker"))
-    images.set(setOf(
-            "allure/${project.name}:${project.version}"
-    ))
+    images.add("allure/${project.name}:${project.version}")
 }
 
 repositories {
@@ -59,13 +57,13 @@ repositories {
 }
 
 dependencies {
-    annotationProcessor("org.projectlombok:lombok:1.18.28")
-    compileOnly("org.projectlombok:lombok:1.18.28")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+    compileOnly("org.projectlombok:lombok:1.18.30")
 
-    annotationProcessor("info.picocli:picocli-codegen:4.1.4")
-    implementation("info.picocli:picocli:4.1.4")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.5")
+    implementation("info.picocli:picocli:4.7.5")
 
-    implementation("org.springframework.ldap:spring-ldap-core:2.3.8.RELEASE")
+    implementation("org.springframework.ldap:spring-ldap-core:3.2.0")
 
     implementation("com.openhtmltopdf:openhtmltopdf-pdfbox:1.0.10")
     implementation("com.openhtmltopdf:openhtmltopdf-core:1.0.10")
@@ -79,49 +77,3 @@ dependencies {
     implementation("io.qameta.allure:allure-model:2.24.0")
     implementation("io.qameta.allure:allure-ee-client:3.53.0")
 }
-
-graalvmNative {
-    toolchainDetection.set(true)
-    binaries {
-        named("main") {
-            useFatJar.set(true)
-            verbose.set(true)
-
-            javaLauncher.set(javaToolchains.launcherFor {
-                languageVersion.set(JavaLanguageVersion.of(17))
-            })
-            requiredVersion.set("23.0.2")
-
-            metadataRepository {
-                enabled.set(true)
-                version.set("0.3.5")
-            }
-
-            configurationFileDirectories.from(
-                    file("src/main/resources/META-INF/native-image")
-            )
-
-            var osArch = System.getProperty("os.arch")
-            println(osArch)
-            var gcFlag = ""
-            if (OperatingSystem.current().isMacOsX() || OperatingSystem.current().isLinux()) {
-                gcFlag = "--gc=G1"
-            }
-            buildArgs.addAll(
-                    "-march=native",
-                    "-R:MinHeapSize=400m",
-                    "-R:MaxHeapSize=400m",
-                    "-H:+AddAllCharsets",
-                    gcFlag,
-            )
-            jvmArgs.addAll(
-                    "-Dfile.encoding=UTF-8",
-                    "-Dsun.jnu.encoding=UTF-8",
-                    "-Djava.awt.headless=true",
-                    "-Djava.net.preferIPv4Stack=true",
-                    "-Djava.net.preferIPv4Addresses=true"
-            )
-        }
-    }
-}
-
